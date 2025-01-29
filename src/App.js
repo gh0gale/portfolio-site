129ee421609721bc47fd31c0fa3a2f6164fd74b9
gh0gale <info.ghogale@gmail.com>
2025-01-29 16:44:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import Projects from './components/Project/Project';
+import Skills from './components/Skill/Skills';
+
+
+function App() {
+  return (
+    <>
+      <Hero/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
+      <Footer/>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Project/Project.js
@@ -0,0 +1,62 @@
+import styles from './ProjectStyles.module.css';
+import nft from '../../assets/nft.png';
+import ll from '../../assets/ll.png';
+import pokedex from '../../assets/pokedex.png';
+import coffee from '../../assets/coffee.png';
+import food from '../../assets/food.png';
+import uber from '../../assets/uber.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={nft}
+          link="https://github.com/gh0gale/nft-landing-page"
+          h3="Blobble"
+          p="HTML/CSS/React.js"
+        />
+        <ProjectCard
+          src={ll}
+          link="https://github.com/gh0gale/little-lemon"
+          h3="Little Lemon"
+          p="HTML/CSS/React.js"
+        />
+        <ProjectCard
+          src={pokedex}
+          link="https://github.com/gh0gale/pokedex"
+          h3="Pokédex"
+          p="HTML/CSS/JavaScript/PokéAPI"
+        />
+      </div>
+
+      <h1 className="sectionTitle">Designs</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={coffee}
+          link="https://www.figma.com/design/KJZEmHDnNvxwRxKpjgL9zt/Coffee-shop-app?node-id=0-1&t=wljVURIKKZOhAe3V-1"
+          h3="Coffee Shop App"
+          p="Figma"
+        />
+        <ProjectCard
+          src={food}
+          link="https://motiff.com/file/82IyyG08UKQ4wDk2JZ6sI83?nodeId=0%3A1&type=design"
+          h3="Food Relief"
+          p="Motiff"
+        />
+        <ProjectCard
+          src={uber}
+          link="https://www.figma.com/design/ntmV5MeyWfnAJsbOI6bKxm/Uber?node-id=0-1&t=hLlvIrA6ZN4Jtpp2-1"
+          h3="Uber Site"
+          p="Figma"
+        />
+      </div>
+    </section>
+
+    
+  );
+}
+
+export default Projects;
