8e0302ede0f3bd9d7e404108a86aad46d3e749df
gh0gale <info.ghogale@gmail.com>
2025-08-05 20:29:17 +0530

    about me changed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,11 @@ export const About = () => {
     <div className={styles.aboutc}>
       <h1 className={styles.abouth}>About Me</h1>
       <p className={styles.aboutp}>
-        
+        I build and optimize digital experiences with the MERN stack, then
+        leverage data analytics and machine learning to make those applications
+        truly intelligent.
         <br />
         <br />
-        
       </p>
     </div>
   );
